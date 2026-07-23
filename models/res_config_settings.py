597ee3996a07527d045a84d63f281b168ade144f
ramya234234 <154891986+ramya234234@users.noreply.github.com>
2026-07-23 17:30:04 +0530

    Delete models directory

--- models/res_config_settings.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    bizvibe_api_key = fields.Char(string="BizVibe API Key", config_parameter='bizvibe.api_key')
-    bizvibe_api_endpoint = fields.Char(string="BizVibe API Endpoint", default="https://api.bizvibe.com", config_parameter='bizvibe.api_endpoint')
